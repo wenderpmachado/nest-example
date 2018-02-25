754f661ffd733a7a2e4a685f10353da99e59a91d
Wender Machado <wenderpmachado@gmail.com>
2018-02-25 18:15:38 -0300

    test the e2e test (fail yet)

--- e2e/user/user.e2e-spec.ts
+++ e2e/user/user.e2e-spec.ts
@@ -5,10 +5,19 @@ import { UserModule } from '../../src/user/user.module';
 import { UserService } from '../../src/user/user.service';
 import { ApplicationModule } from '../../src/app.module';
 import { Repository } from 'typeorm';
+import { User } from '../../src/user/user.entity';
 
 describe('User', () => {
     const server = express();
-    const userService = { findAll: () => ['test'] };
+    let defaultUser: User = new User(0, 'Wender', true);
+    const changedUser: User = new User(0, 'Wender Pinto Machado', false);
+    const userService = {
+        create: (user: User) => { defaultUser.id = 1; changedUser.id = 1; return defaultUser; },
+        findAll: () => [defaultUser],
+        findById: (id: number) => defaultUser,
+        update: (user: User) => changedUser,
+        delete: (id: number) => { true }
+    };
 
     beforeAll(async () => {
         const module = await Test.createTestingModule({
@@ -27,12 +36,48 @@ describe('User', () => {
         await app.init();
     });
 
+    it(`/POST user`, () => {
+        return request(server)
+            .post('/user', defaultUser)
+            .expect(200)
+            .expect({
+                data: userService.create(defaultUser),
+            });
+    });
+
     it(`/GET user`, () => {
         return request(server)
             .get('/user')
             .expect(200)
             .expect({
                 data: userService.findAll(),
+            });
+    });
+
+    it(`/GET/1 user`, () => {
+        return request(server)
+            .get('/user/1')
+            .expect(200)
+            .expect({
+                data: userService.findById(defaultUser.id),
+            });
+    });
+
+    it(`/PUT user`, () => {
+        return request(server)
+            .put('/user', changedUser)
+            .expect(200)
+            .expect({
+                data: userService.update(changedUser),
+            });
+    });
+
+    it(`/DELETE user`, () => {
+        return request(server)
+            .delete('/user/1')
+            .expect(200)
+            .expect({
+                data: userService.delete(defaultUser.id),
             });
     });
 });
